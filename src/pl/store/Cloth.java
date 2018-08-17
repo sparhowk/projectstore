1237719c5c37379955f215cf502f83aba2af0c8a
sparhowk <sparhowk@wp.pl>
2018-08-17 14:52:46 +0200

    Add class Cloth

--- src/pl/store/Cloth.java
+++ src/pl/store/Cloth.java
@@ -1,4 +1,48 @@
 package pl.store;
 
-public class Cloth {
+public class Cloth extends Product{
+    private String size;
+    private String material;
+
+    public Cloth(ClothBuldier buldier){
+        super(buldier);
+        this.size = buldier.size;
+        this.material = buldier.material;
+    }
+
+    public static class ClothBuldier extends Product.Buldier {
+        private String size;
+        private String material;
+
+        public ClothBuldier() {}
+
+        public Buldier setSize(String size) {
+            this.size = size;
+            return this;
+        }
+
+        public Buldier setMaterial(String material) {
+            this.material = material;
+            return this;
+        }
+
+        public Cloth bulid() {
+            return new Cloth(this);
+        }
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()
+                + ", size=" + size
+                + ", Material=" + material;
+    }
 }
